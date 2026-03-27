de27fa6b86799bd9f912126ad77e3b5454e0cd7e
TINGPING <waiwaichan212@gmail.com>
2026-03-27 20:18:00 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,1 @@
-const greeting = document.getElementById('greeting');
-const btn = document.getElementById('btn');
-
-btn.addEventListener('click', function () {
-  greeting.textContent = greeting.textContent === 'Hello World'
-    ? '你好，世界！'
-    : 'Hello World';
-});
+// 預留互動邏輯
